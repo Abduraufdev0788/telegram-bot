06d9b01574ee6efeb5e4db748995aab1c2373329
abduraufdev0788 <abduraufnasrullayev790@gmail.com>
2025-08-03 17:29:19 +0500

    bajarildi

--- handlers.py
+++ handlers.py
@@ -19,3 +19,43 @@ def help(update: Update, context: CallbackContext):
 
 def echo(update: Update, context: CallbackContext):
     update.message.reply_text(update.message.text)
+    
+def echo_audio(update: Update, context: CallbackContext):
+    audio = update.message.audio
+    update.message.reply_audio(audio=audio)
+
+def echo_photo(update: Update, context: CallbackContext):
+    photo = update.message.photo[0]
+    update.message.reply_photo(photo=photo)
+
+def echo_doc(update:Update, context:CallbackContext):
+    doc = update.message.document
+    update.message.reply_document(document=doc)
+
+def echo_voice(update:Update, context:CallbackContext):
+    voice = update.message.voice
+    update.message.reply_voice(voice=voice)
+
+def echo_stiker(update:Update, context : CallbackContext):
+    stiker = update.message.sticker
+    update.message.reply_sticker(sticker=stiker)
+
+def echo_contact(update:Update, context : CallbackContext):
+    contact = update.message.contact
+    update.message.reply_contact(contact=contact)
+
+def echo_video(update:Update, context: CallbackContext):
+    video = update.message.video
+    update.message.reply_video(video=video)
+
+def echo_anim(update :Update, context:CallbackContext):
+    anim = update.message.animation
+    update.message.reply_animation(animation=anim)
+
+def echo_locat(update:Update, context:CallbackContext):
+    location = update.message.location
+    update.message.reply_location(location=location)
+
+def echo_dice(update:Update, context:CallbackContext):
+    dice = update.message.dice.emoji
+    update.message.reply_dice(emoji=dice)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 from config import TOKEN
 
-from handlers import start, help, echo
+from handlers import start, help, echo, echo_audio,echo_photo,echo_doc, echo_voice, echo_stiker, echo_contact, echo_video,echo_anim, echo_locat, echo_dice
 
 
 def main() -> None:
@@ -15,6 +15,16 @@ def main() -> None:
 
     # message handler
     dispatcher.add_handler(MessageHandler(Filters.text, echo))
+    dispatcher.add_handler(MessageHandler(Filters.audio, echo_audio))
+    dispatcher.add_handler(MessageHandler(Filters.photo, echo_photo))
+    dispatcher.add_handler(MessageHandler(Filters.document, echo_doc))
+    dispatcher.add_handler(MessageHandler(Filters.voice, echo_voice))
+    dispatcher.add_handler(MessageHandler(Filters.sticker, echo_stiker))
+    dispatcher.add_handler(MessageHandler(Filters.contact, echo_contact))
+    dispatcher.add_handler(MessageHandler(Filters.video, echo_video))
+    dispatcher.add_handler(MessageHandler(Filters.animation, echo_anim))
+    dispatcher.add_handler(MessageHandler(Filters.location, echo_locat))
+    dispatcher.add_handler(MessageHandler(Filters.dice, echo_dice))
 
     updater.start_polling()
     updater.idle()
